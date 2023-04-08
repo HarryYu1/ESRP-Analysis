0537be70fd5cdfbd20491a8a49a2dc96deb52590
HarryYu1 <122706464+HYUpersonal@users.noreply.github.com>
2023-04-08 15:38:00 -0500

    finished calculations

--- analysis.py
+++ analysis.py
@@ -2,6 +2,18 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 
+
+
+#########################################################################################################################
+#
+#   Author : Harry Yu
+#   Date : 04/08/2023
+#   Name : ESRP Analysis Program
+#   Desc : Sorts through the mappings of treatment and background and computes the adjusted lead per potassium level
+#
+#########################################################################################################################
+
+
 #reconstruction of my original code that got deleted
 
 df = pd.read_csv('ROIs.xlsx_-_Sheet1_1.csv')
@@ -29,7 +41,7 @@ df['sample_num'] = df['sample_num'].astype(int)
 
 #subtract based off of values in ADJUST_BG
 #there will be both a green column and a blue column
-i=0
+
 lead_mean_list = []
 potassium_mean_list = []
 for label, Series in mappings.iterrows():
@@ -41,21 +53,37 @@ for label, Series in mappings.iterrows():
     lead_mean = np.mean(lead_values)
     lead_mean_list.append(lead_mean)
 
-    i += 1
+    #DO NOT subtract the background from the control
+    if 'Control' in Series['treatment']:
+        print('control: ' + Series['treatment'])
+        potassium_values = df.loc[(df['sample_num'] == Series['sample_num'])]['K-mean[ug/cm2]'].to_numpy()
+    else:
+        print('not control: ' + Series['treatment'])
+        potassium_values = df.loc[(df['sample_num'] == Series['sample_num'])]['K-mean[ug/cm2]'].to_numpy() - dfBackground.loc[dfBackground['sample_num'] == Series['bg_num_red']]['K-mean[ug/cm2]'].to_numpy().item()
+    potassium_values = potassium_values[potassium_values != 0]
+    potassium_mean = np.mean(potassium_values)
+    potassium_mean_list.append(potassium_mean)
+
 
 #insert into mappings
 mappings.insert(4, "lead_no_background", lead_mean_list)
+mappings.insert(5, "potassium_no_background", potassium_mean_list)
+mappings['adjusted_lead'] = mappings['lead_no_background']/mappings['potassium_no_background']
+
+
+#post-process (sort by treatment)
+mappings_by_plant = mappings.sort_values(by=['plant'], ascending=True)
+mappings_by_treatment = mappings.sort_values(by=['treatment'], ascending=True)
 
 #replaced 72 with 71 due to lack of 71
 #42 with 41 in the series sample num
-#print((dfBackground.loc[dfBackground['sample_num'] == 72])['Pb_L-mean[ug/cm2]'].to_numpy())
 
-#final calculation
 
 
 #write to file
-#df.to_csv("out.csv")
-#dfBackground.to_csv("background_values.csv")
+mappings_by_plant.to_csv('mappings_by_plant.csv')
+mappings_by_treatment.to_csv('mappings_by_treatment.csv')
 
-#print(df.head(5))
-print(mappings.head(20))
+
+print(mappings_by_plant.head(10))
+print(mappings_by_treatment.head(10))
